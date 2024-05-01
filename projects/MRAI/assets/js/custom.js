24797ab9928def09483e87fa007d1d69988befe3
wubeijing <wubeijing@yahoo.com>
2024-04-30 22:12:19 -0700

    add

---
+++ projects/MRAI/assets/js/custom.js
@@ -0,0 +1,148 @@
+/**	
+	Custom JS
+	
+	1. FIXED MENU
+	2. FEATURED SLIDE ( SLICK SLIDER )
+	3. MENU SMOOTH SCROLLING
+	4. PORTFOLIO GALLERY
+	6. PORTFOLIO POPUP VIEW ( IMAGE LIGHTBOX )
+	7. CLIENT TESTIMONIALS ( SLICK SLIDER )
+	
+**/
+
+
+
+(function( $ ){
+
+
+	/* ----------------------------------------------------------- */
+	/*  2. FIXED MENU
+	/* ----------------------------------------------------------- */
+
+
+	jQuery(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 150) {
+        $('#mu-header').addClass('mu-fixed-nav');
+        
+	    } else {
+	        $('#mu-header').removeClass('mu-fixed-nav');
+	    }
+	});
+
+		
+	/* ----------------------------------------------------------- */
+	/*  2. FEATURED SLIDE (SLICK SLIDER)
+	/* ----------------------------------------------------------- */
+
+		$('.mu-featured-slide').slick({
+		  arrows: false,
+		  dots: true,
+		  infinite: true,
+		  speed: 500,
+		  autoplay: true,
+		  cssEase: 'linear'
+		});
+
+		 // Smooth scrolling for anchor links
+		 $('a[href*=#]').on('click', function(e) {
+			e.preventDefault();
+			$('html, body').animate({
+				scrollTop: $($(this).attr('href')).offset().top
+			}, 500, 'linear');
+		});
+
+	/* ----------------------------------------------------------- */
+	/*  3. MENU SMOOTH SCROLLING
+	/* ----------------------------------------------------------- */ 
+
+		//MENU SCROLLING WITH ACTIVE ITEM SELECTED
+
+		// Cache selectors
+		var lastId,
+		topMenu = $(".mu-menu"),
+		topMenuHeight = topMenu.outerHeight()+13,
+		// All list items
+		menuItems = topMenu.find('a[href^=\\#]'),
+		// Anchors corresponding to menu items
+		scrollItems = menuItems.map(function(){
+		  var item = $($(this).attr("href"));
+		  if (item.length) { return item; }
+		});
+
+		// Bind click handler to menu items
+		// so we can get a fancy scroll animation
+		menuItems.click(function(e){
+		  var href = $(this).attr("href"),
+		      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+22;
+		  jQuery('html, body').stop().animate({ 
+		      scrollTop: offsetTop
+		  }, 1500);
+		  e.preventDefault();
+		});
+
+		// Bind to scroll
+		jQuery(window).scroll(function(){
+		   // Get container scroll position
+		   var fromTop = $(this).scrollTop()+topMenuHeight;
+		   
+		   // Get id of current scroll item
+		   var cur = scrollItems.map(function(){
+		     if ($(this).offset().top < fromTop)
+		       return this;
+		   });
+		   // Get the id of the current element
+		   cur = cur[cur.length-1];
+		   var id = cur && cur.length ? cur[0].id : "";
+		   
+		   if (lastId !== id) {
+		       lastId = id;
+		       // Set/remove active class
+		       menuItems
+		         .parent().removeClass("active")
+		         .end().filter("[href=\\#"+id+"]").parent().addClass("active");
+		   }           
+		})
+
+
+	/* ----------------------------------------------------------- */
+	/*  4. PORTFOLIO GALLERY
+	/* ----------------------------------------------------------- */ 
+		$('.filtr-container').filterizr();
+
+		//Simple filter controls
+
+	    $('.mu-simplefilter li').click(function() {
+	        $('.mu-simplefilter li').removeClass('active');
+	        $(this).addClass('active');
+	    });
+	
+
+	/* ----------------------------------------------------------- */
+	/*  6. PORTFOLIO POPUP VIEW ( IMAGE LIGHTBOX )
+	/* ----------------------------------------------------------- */ 
+
+	$('.mu-imglink').magnificPopup({
+	  type: 'image',
+	  mainClass: 'mfp-fade',
+	  gallery:{
+	    enabled:true
+	  }
+	});
+
+	/* ----------------------------------------------------------- */
+	/*  7. CLIENT TESTIMONIALS (SLICK SLIDER)
+	/* ----------------------------------------------------------- */
+
+		$('.mu-testimonial-slide').slick({
+		  arrows: false,
+		  dots: true,
+		  infinite: true,
+		  speed: 500,
+		  autoplay: true,
+		  cssEase: 'linear'
+		});
+
+
+	
+	
+})( jQuery );
